18af95eb4575e9de1ec1b8d21ea522784ff091fe
Rayhan Al Kavey <91812273+RayhanalKavey@users.noreply.github.com>
2024-01-30 20:59:08 +0600

    Setup prisma and clasname package.

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,39 @@
+"use client"; //We use client mood because of currentPath. It is browser based.
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
-
+import { AiOutlineHeatMap } from "react-icons/ai";
 const NavBar = () => {
+  const currentPath = usePathname();
+  //   console.log(currentPath);
+  const links = [
+    { label: "Dashboard", link: "/" },
+    { label: "Issues", link: "/issues" },
+  ];
   return (
     <nav className="flex gap-x-6 border-b h-14 items-center px-6 mb-5">
-      <Link href="/">Logo</Link>
+      <Link href="/">
+        <AiOutlineHeatMap className="text-2xl" />
+      </Link>
       <ul className="flex gap-x-6">
-        <li>Dashboard</li>
-        <li>Issues</li>
+        {links?.map((singleLInk) => (
+          <li
+            key={singleLInk.link}
+            // className={`${
+            //   currentPath === singleLInk.link
+            //     ? `text-zinc-800`
+            //     : "text-zinc-500"
+            // }  hover:text-zinc-800 transition-colors`}
+            className={classNames({
+              "text-zinc-800": currentPath === singleLInk.link,
+              "text-zinc-500": currentPath !== singleLInk.link,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+          >
+            <Link href={singleLInk.link}>{singleLInk.label}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );

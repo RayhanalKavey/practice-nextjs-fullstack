97b76d8543d346e88276dbb7d9f3405ece529120
Rayhan Al Kavey <91812273+RayhanalKavey@users.noreply.github.com>
2024-02-02 19:11:48 +0600

    React Hook Form AND Axios (POST issues and redirect to the issue page with useRouter from next/navigation)

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,21 +1,48 @@
 "use client";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
 import { CiSearch } from "react-icons/ci";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  console.log(register);
   return (
-    <div className="max-w-xl mx-auto space-y-3">
+    <form
+      className="max-w-xl mx-auto space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        const response = await axios.post("/api/issues", data);
+        console.log("response", response.data);
+        alert(response.statusText);
+        console.log("response", response.statusText);
+        router.push("/issues");
+      })}
+    >
       <h1>New Issue Page</h1>
       <TextField.Root>
         <TextField.Slot>
           <CiSearch height="16" width="16" />
         </TextField.Slot>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
